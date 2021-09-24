7e52316a105b91dfdc95aa5d6c30ae7996a5d918
İsmail Can Şeker <icanseker@gmail.com>
2021-09-24 22:03:59 +0300

    Update README.md

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Currently supported shell commands: (The project is still in the works.)
 Date command example:
 
 ```java
-package ex.unix.shell.cmd.date;
+import unix.shell.cmd.core.date.Date;
 
 ...
 
@@ -44,4 +44,46 @@ package ex.unix.shell.cmd.date;
 The output is
 ```
 date --date='15 Dec 2013'
+```
+
+...
+Other date str specifying examples:
+
+```java
+import unix.shell.cmd.core.date.Date;
+
+...
+
+	Date date = new Date();
+
+		// day
+		date.specify("15 Dec 2013");
+		date.specify("15dec2013");
+		date.specify("15-dec-2013");
+		date.specify("24 September 2003");
+		date.specify("Sep 24, 2003");
+		date.specify("2004-02-29");
+		date.specify("72-9-24"); // Assume 19xx for 69 through 99
+		date.specify("45-9-24"); // 20xx for 00 through 68
+		date.specify("9/24/72");
+
+		// The year can also be omitted. In this case, the last specified year is used, or the current year if none.
+		date.specify("9/24");
+		date.specify("sep24"); // or "sep 24"
+
+		// time
+		date.specify("19:59"); // = 19:59:00
+		date.specify("19:59:34");
+		date.specify("08:52 a.m."); // or am
+		date.specify("08:52pm"); // or p.m.
+
+		// combine both day and time
+		date.specify("2004-02-29 08:52pm"); // or p.m.
+		date.specify("2004-02-29 19:59:34");
+
+		// time zone
+		date.specify("TZ=\"Europe/Paris\"");
+
+		// combine time zone and day and/or time
+		date.specify("TZ=\"Europe/Paris\" 24 September 2003 08:52pm"); // meaning: What date is it if it's September 24, 2003, at 8:52 p.m. in Europe/Paris?
 ```
--- src/ex/unix/shell/cmd/date/Example.java
+++ src/ex/unix/shell/cmd/date/Example.java
@@ -18,37 +18,37 @@ public class Example {
 
 		// day
 		date.specify("15 Dec 2013");
-//		date.specify("15dec2013");
-//		date.specify("15-dec-2013");
-//		date.specify("24 September 2003");
-//		date.specify("Sep 24, 2003");
-//		date.specify("2004-02-29");
-//		date.specify("72-9-24"); // Assume 19xx for 69 through 99
-//		date.specify("45-9-24"); // 20xx for 00 through 68
-//		date.specify("9/24/72");
-//
-//		// The year can also be omitted. In this case, the last specified year is used,
-//		// or the current year if none.
-//		date.specify("9/24");
-//		date.specify("sep24"); // or "sep 24"
-//
-//		// time
-//		date.specify("19:59"); // = 19:59:00
-//		date.specify("19:59:34");
-//		date.specify("08:52 a.m."); // or am
-//		date.specify("08:52pm"); // or p.m.
-//
-//		// combine both day and time
-//		date.specify("2004-02-29 08:52pm"); // or p.m.
-//		date.specify("2004-02-29 19:59:34");
-//
-//		// time zone
-//		date.specify("TZ=\"Europe/Paris\"");
-//
-//		// combine time zone and day and/or time
-//		date.specify("TZ=\"Europe/Paris\" 24 September 2003 08:52pm"); // meaning: What date is it if it's September
-//																		// 24, 2003, at 8:52 p.m. in Europe/Paris?
-//		date.specify("TZ=\"Africa/Addis_Ababa\" 2004-10-31 06:30");
+		date.specify("15dec2013");
+		date.specify("15-dec-2013");
+		date.specify("24 September 2003");
+		date.specify("Sep 24, 2003");
+		date.specify("2004-02-29");
+		date.specify("72-9-24"); // Assume 19xx for 69 through 99
+		date.specify("45-9-24"); // 20xx for 00 through 68
+		date.specify("9/24/72");
+
+		// The year can also be omitted. In this case, the last specified year is used,
+		// or the current year if none.
+		date.specify("9/24");
+		date.specify("sep24"); // or "sep 24"
+
+		// time
+		date.specify("19:59"); // = 19:59:00
+		date.specify("19:59:34");
+		date.specify("08:52 a.m."); // or am
+		date.specify("08:52pm"); // or p.m.
+
+		// combine both day and time
+		date.specify("2004-02-29 08:52pm"); // or p.m.
+		date.specify("2004-02-29 19:59:34");
+
+		// time zone
+		date.specify("TZ=\"Europe/Paris\"");
+
+		// combine time zone and day and/or time
+		date.specify("TZ=\"Europe/Paris\" 24 September 2003 08:52pm"); // meaning: What date is it if it's September
+																		// 24, 2003, at 8:52 p.m. in Europe/Paris?
+		date.specify("TZ=\"Africa/Addis_Ababa\" 2004-10-31 06:30");
 //
 //		// define time zone with TimeZone class
 //		date.specify("24 September 2003 08:52pm", TimeZone.EUROPE.PARIS);
